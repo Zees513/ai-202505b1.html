b835661e0497f151527eab516fd8a4f50dba750c
unknown <zk.work99@gmail.com>
2025-08-11 23:03:27 -0700

    now

---
+++ assets/assets/js/app.js
@@ -0,0 +1,13 @@
+//if else
+let num = +prompt("Enter your number");
+
+if(num  <0){
+    document.write("this is negative number")
+}
+else{
+    if(num >0){
+    document.write("this is positive number")
+    }
+}
+
+    
