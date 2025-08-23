c6a45251f9b21fd1442362ed14516974c2bbeafc
unknown <zk.work99@gmail.com>
2025-08-22 22:57:46 -0700

    kl

--- js/loop.js
+++ js/loop.js
@@ -1,10 +1,15 @@
-let a = prompt("starting number");
-let b = prompt("ending number")
-for( let i=(a); i<=(b); i++ ) {
-    document.write( i, "- this is for loop <br>")
-}
+// let a = prompt("starting number");
+// let b = prompt("ending number")
+// for( let i=(a); i<=(b); i++ ) {
+//     document.write( i, "- this is for loop <br>")
+// }
 
 
 // for( let i=10; i>=1; i-- ) {
 //     document.write( i, "- this is for loop <br>")
 // }
+
+let start = prompt("enter a number for table")
+for(let i=1;i<=10;i++ ){
+    document.write(start,"X",i,"=", start*i, "<br>")
+}
