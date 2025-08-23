2f8d850ac64082e6d59f92b4996eee53911cdc15
unknown <zk.work99@gmail.com>
2025-08-22 22:31:10 -0700

    yes

---
+++ js/loop.js
@@ -0,0 +1,10 @@
+let a = prompt("starting number");
+let b = prompt("ending number")
+for( let i=(a); i<=(b); i++ ) {
+    document.write( i, "- this is for loop <br>")
+}
+
+
+// for( let i=10; i>=1; i-- ) {
+//     document.write( i, "- this is for loop <br>")
+// }
