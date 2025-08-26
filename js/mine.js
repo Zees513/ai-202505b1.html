3b87db7d83fe11807fbf13d9df3ed34669e242be
unknown <zk.work99@gmail.com>
2025-08-25 21:40:08 -0700

    kl

---
+++ js/mine.js
@@ -0,0 +1,8 @@
+// do while loop 
+
+let a = prompt("enter a number");
+let b = prompt("enter a printing number")
+do {
+    document.write (a,"-","this is do while loop <br>")
+    a++
+}while(a<=(b))
