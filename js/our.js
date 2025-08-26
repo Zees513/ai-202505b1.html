dceeb4e1c5b5d5c86b104c641765fbfc57b23ad5
unknown <zk.work99@gmail.com>
2025-08-25 22:40:36 -0700

    noe

---
+++ js/our.js
@@ -0,0 +1,13 @@
+// array
+
+
+let Name = ["zain","ali","zeeshan","hamza"]
+document.write(Name[3] ,  "<br>")
+
+
+
+let courses = new Array("CPISM","DISM","ADSE","HDSE")
+document.write(courses[2])
+
+
+document.write(courses.length)
